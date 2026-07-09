5a359c58b052a1f8c594c7eca0da083f446daa8b
JahamisD <jamesartinosanto77@gmail.com>
2026-07-09 08:11:48 +0800

    Mobile updated

--- backend/src/main/java/cit/edu/inosanto/backend/CarGo/features/bookings/BookingService.java
+++ backend/src/main/java/cit/edu/inosanto/backend/CarGo/features/bookings/BookingService.java
@@ -3,9 +3,8 @@ package cit.edu.inosanto.backend.CarGo.features.bookings;
 public class BookingService {
     private final BookingRepository bookingRepository;
 
-    public BookingService(BookingRepository bookingRepository){
+    public BookingService(BookingRepository bookingRepository) {
         this.bookingRepository = bookingRepository;
     }
-
 
 }
